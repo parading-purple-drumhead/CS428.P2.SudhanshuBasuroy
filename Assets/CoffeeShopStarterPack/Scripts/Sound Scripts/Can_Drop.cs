ad48547b8155d88960816bd781c7bf46e7d22599
advaitpai <67473396+advaitpai@users.noreply.github.com>
2022-10-10 01:44:49 -0500

    Coffee Scene Update

--- Assets/CoffeeShopStarterPack/Scripts/Sound Scripts/Can_Drop.cs
+++ Assets/CoffeeShopStarterPack/Scripts/Sound Scripts/Can_Drop.cs
@@ -1,47 +1,49 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class Can_Drop : MonoBehaviour
-// {
-//     public AudioSource can_sound;
-
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         can_sound = GetComponent<AudioSource>();
-//     }
-
-//     void OnCollisionEnter(Collision collision)
-//     {
-//         // foreach (ContactPoint contact in collision.contacts)
-//         // {
-//         //     Debug.DrawRay(contact.point, contact.normal, Color.white);
-//         // }
-//         // if (collision.relativeVelocity.magnitude > 2) can_sound.Play();
-//         Debug.Log($"{gameObject.name} is colliding with {collision.collider.name}");
-//         if(!can_sound.isPlaying){
-//             can_sound.Play();
-//         }
-//     }
-// }
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using Zinnia.Tracking.Collision;
 
 public class Can_Drop : MonoBehaviour
 {
     public AudioSource can_sound;
+
+    // Start is called before the first frame update
     void Start()
     {
-        can_sound = GetComponent<AudioSource>(); 
+        can_sound = GetComponent<AudioSource>();
     }
-    public void Bounce(CollisionNotifier.EventData data)
+
+    void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Colllision Detected");
-        if(!can_sound.isPlaying)
-        {
-            can_sound.Play();
+        // foreach (ContactPoint contact in collision.contacts)
+        // {
+        //     Debug.DrawRay(contact.point, contact.normal, Color.white);
+        // }
+        // if (collision.relativeVelocity.magnitude > 2) can_sound.Play();
+        Debug.Log($"{gameObject.name} is colliding with {collision.collider.name}");
+        if (collision.relativeVelocity.magnitude > 0.5) {
+            if (!can_sound.isPlaying) {
+                can_sound.Play();
+            }
         }
     }
 }
+
+// using UnityEngine;
+// using Zinnia.Tracking.Collision;
+
+// public class Can_Drop : MonoBehaviour
+// {
+//     public AudioSource can_sound;
+//     void Start()
+//     {
+//         can_sound = GetComponent<AudioSource>(); 
+//     }
+//     public void Bounce(CollisionNotifier.EventData data)
+//     {
+//         Debug.Log("Colllision Detected");
+//         if(!can_sound.isPlaying)
+//         {
+//             can_sound.Play();
+//         }
+//     }
+// }
