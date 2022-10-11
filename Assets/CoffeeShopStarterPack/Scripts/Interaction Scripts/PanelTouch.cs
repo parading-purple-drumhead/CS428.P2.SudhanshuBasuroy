0df67ab0dcb3feec5981fb1f9601edfdc424da29
advaitpai <67473396+advaitpai@users.noreply.github.com>
2022-10-11 01:17:08 -0500

    Bill Panel Logic Started

---
+++ Assets/CoffeeShopStarterPack/Scripts/Interaction Scripts/PanelTouch.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using Zinnia.Tracking.Collision;
+
+public class PanelTouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource panel_sound;
+    public bool generating_bill;
+    void Start()
+    {
+        // Debug.Log("Item Touched!!!!");
+    }
+
+    // Update is called once per frame
+    public void panel_collision(CollisionNotifier.EventData data)
+    {
+        Debug.Log("Panel Touched!!!!");
+        if(!panel_sound.isPlaying)
+        {
+            panel_sound.Play();
+        }
+        
+    }
+}
--- Assets/CoffeeShopStarterPack/Scripts/Sound Scripts/Can_Drop.cs
+++ Assets/CoffeeShopStarterPack/Scripts/Sound Scripts/Can_Drop.cs
@@ -10,17 +10,11 @@ public class Can_Drop : MonoBehaviour
     void Start()
     {
         // can_sound = GetComponent<AudioSource>();
-         Debug.Log("Code Acccessed");
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        // foreach (ContactPoint contact in collision.contacts)
-        // {
-        //     Debug.DrawRay(contact.point, contact.normal, Color.white);
-        // }
-        // if (collision.relativeVelocity.magnitude > 2) can_sound.Play();
-        Debug.Log("Hand Collision");
+        // Debug.Log("Hand Collision");
         if (collision.relativeVelocity.magnitude > 3) {
             if (!can_sound.isPlaying) {
                 Debug.Log($"{gameObject.name} is colliding with {collision.collider.name} with velocity {collision.relativeVelocity.magnitude} ");
