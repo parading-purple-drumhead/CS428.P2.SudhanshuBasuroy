030d098ee7ebf14e37bec62845353b45c26d28bf
advaitpai <67473396+advaitpai@users.noreply.github.com>
2022-10-11 20:09:00 -0500

    Update CoffeeScene.unity

---
+++ Assets/CoffeeShopStarterPack/Scripts/CreateNewOjects/SpawnBill.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBill : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public Vector3 spawnPosition;
+
+    public void OnSpawnPrefab()
+    {
+        Debug.Log("Object created");
+
+        // Debug
+        //     .Log("Cube's position: " +
+        //     reference.transform.position.x +
+        //     " " +
+        //     reference.transform.position.y +
+        //     " " +
+        //     reference.transform.position.z);
+        var created = Instantiate(prefab, spawnPosition, Quaternion.identity);
+        Debug.Log(created.transform.position);
+    }
+
+    private void Start()
+    {
+        OnSpawnPrefab();
+    }
+}
